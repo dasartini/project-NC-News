4943553b6dac82ced9671db720bed4774978d2fc
dasartini <adriansartinid@gmail.com>
2024-04-15 17:06:45 +0100

    done task 4

--- MVC/app.js
+++ MVC/app.js
@@ -1,14 +1,27 @@
 const express = require('express')
 const app = express()
-
-
-// my different cb functions form controllers
-const {getTopics, beOnApi} = require('./controllers')
+const {getTopics, getApiEndPoints, getArticleById} = require('./controllers')
 
 app.use(express.json())
 
 app.get('/api/topics' , getTopics)
-app.get('/api', beOnApi)
+app.get('/api', getApiEndPoints)
+app.get('/api/articles/:article_id', getArticleById )
+
+app.use((err, req, res, next) => {
+    if (err.status && err.message ) {
+      res.status(err.status).send({ message: err.message });
+    } 
+    else next(err);
+  });
+
+app.use( (err, req, res, next)=>{
+    if(err.code === "22P02"){
+        res.status(400).send({message : "Bad request :("})
+    }
+    else next(err)
+})
+
 
 app.all('*' ,(req, res)=>{
     res.status(404).send({message : "Error 404! endpoint not found :("})
--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -4,26 +4,26 @@ const testData = require('../db/data/test-data');
 const request = require("supertest");
 const app = require('../MVC/app')
 
-beforeEach(() => { return seed(testData)});
+beforeEach(() => { return seed(testData) });
 afterAll(() => db.end());
 
-describe("General not found error",()=>{
-    test('GET ERROR: returns 404 error if there is an invalid point entered.',()=>{
+describe("General not found error", () => {
+    test('GET ERROR: returns 404 error if there is an invalid point entered.', () => {
         return request(app)
-        .get("/notAnyValidEndpoint")
-        .expect(404)
-        .then(({body})=>{
-            const {message} = body;
-            expect(message).toBe("Error 404! endpoint not found :(")
-        });
+            .get("/notAnyValidEndpoint")
+            .expect(404)
+            .then(({ body }) => {
+                const { message } = body;
+                expect(message).toBe("Error 404! endpoint not found :(")
+            });
     });
 });
 
 describe("/api/topics", () => {
-    test(" General health-check test checks api connecting successfully.", ()=>{
+    test(" General health-check test checks api connecting successfully.", () => {
         return request(app)
-        .get('/api/topics')
-        .expect(200)
+            .get('/api/topics')
+            .expect(200)
 
     });
 
@@ -33,36 +33,81 @@ describe("/api/topics", () => {
             .expect(200)
             .then(({ body }) => {
                 const { topics } = body;
-               // console.log('baaaaaaaaaaaaaaaaaaaaaaaaa', topics)
                 expect(topics.length).toBe(3)
                 topics.forEach((topic) => {
                     expect(typeof topic.description).toBe('string')
                     expect(typeof topic.slug).toBe('string')
-                })
+                });
+            });
+
+    });
+    test('ERROR: returns 404 error if there is an invalid point entered.', () => {
+        return request(app)
+            .get("/api/tApics")
+            .expect(404)
+            .then(({ body }) => {
+                const { message } = body;
+                expect(message).toBe("Error 404! endpoint not found :(")
+            });
+    });
+
+});
+
+describe('GET:/api', () => {
+    test("Responds with an object describing all the aviable endpoints in this API", () => {
+        return request(app)
+            .get("/api")
+            .expect(200)
+            .then(({ body }) => {
+                const { endpoint } = body
+                expect(typeof endpoint).toBe("object")
+                expect(endpoint.hasOwnProperty("GET /api")).toBe(true)
+                expect(endpoint.hasOwnProperty("GET /api/topics")).toBe(true)
+                expect(endpoint.hasOwnProperty("GET /api/articles")).toBe(true)
+                expect(endpoint.hasOwnProperty("GET /api/articles/:article_id")).toBe(true)
+            });
+    });
+
+});
+
+describe("/api/articles", () => {
+    test("GET:/api/articles/article:id returns the specified article", () => {
+        return request(app)
+            .get('/api/articles/6')
+            .expect(200)
+            .then(({ body }) => {
+                const { article } = body
+                expect(typeof article).toBe("object")
+                expect(article.hasOwnProperty('article_id')).toBe(true)
+                expect(article.hasOwnProperty('article_img_url')).toBe(true)
+                expect(article.hasOwnProperty('author')).toBe(true)
+                expect(article.hasOwnProperty('body')).toBe(true)
+                expect(article.hasOwnProperty('created_at')).toBe(true)
+                expect(article.hasOwnProperty('title')).toBe(true)
+                expect(article.hasOwnProperty('topic')).toBe(true)
+                expect(article.hasOwnProperty('votes')).toBe(true)
             })
 
     })
-    test('ERROR: returns 404 error if there is an invalid point entered.',()=>{
-        return request(app)
-        .get("/api/tApics")
-        .expect(404)
-        .then(({body})=>{
-            const {message} = body;
-            expect(message).toBe("Error 404! endpoint not found :(")
-        });
-    });
 
-})
-
-describe('Being on api',()=>{
-    test("Responds with an object describing all the aviable endpoints in this API",()=>{
+    test("GET: Returns an error message when provided an invalid article id",()=>{
         return request(app)
-        .get("/api")
-        .expect(200)
-        .then(({body})=>{
-            const {endpoint} = body
-            expect(typeof endpoint).toBe("object")
+        .get('/api/articles/papa_pear')
+        .expect(400)
+        .then(({ body }) => {
+            const { message } = body;
+            expect(message).toBe("Bad request :(")
         })
     })
 
-})
+    test.only("GET: Returns an error message when provided an higher id number",()=>{
+        return request(app)
+        .get('/api/articles/99999999')
+        .expect(400)
+        .then(({ body }) => {
+            const { message } = body;
+            expect(message).toBe("The id provided does not exist!")
+        })
+    })
+
+});
