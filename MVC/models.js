bfafb8439c22a2ea1c178d462c0bf58718234db5
dasartini <adriansartinid@gmail.com>
2024-04-17 11:47:33 +0100

    Modified query to add instead of replace vote value

--- MVC/models.js
+++ MVC/models.js
@@ -60,7 +60,7 @@ function postAComment(article_id, body) {
 }
 
 function votes(article_id, inc_votes) {
-    return db.query(`UPDATE articles SET votes=$1 WHERE article_id =$2 RETURNING *;`, [inc_votes, article_id])
+    return db.query(`UPDATE articles SET votes= votes + $1 WHERE article_id =$2 RETURNING *;`, [inc_votes, article_id])
 }
 
 module.exports = { findTopics, fetchArticleId, fetchArticles, fetchCommentsByArtId, checkIfArticleExist, postAComment, votes }
--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -140,6 +140,27 @@ describe("/api/articles/article:id", () => {
                 })
             })
     });
+
+    test("PATCH: /api/articles/:article_id updates an article by its id.", () => {
+        return request(app)
+            .patch("/api/articles/1")
+            .send({ inc_votes: -99 })
+            .expect(201)
+            .then(({ body }) => {
+                const { article } = body
+                expect(article).toEqual({
+                    article_id: 1,
+                    title: 'Living in the shadow of a great man',
+                    topic: 'mitch',
+                    author: 'butter_bridge',
+                    body: 'I find this existence challenging',
+                    created_at: '2020-07-09T20:11:00.000Z',
+                    votes: 1,
+                    article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
+                })
+            })
+    });
+
     test("PATCH: /api/articles/:article_id updates an article by its id (negative numbers).", () => {
         return request(app)
             .patch("/api/articles/3")
